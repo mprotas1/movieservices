3af535bf890a273ec76588ece0d81da8cc193e01
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-07-02 19:07:43 +0200

    Get user (customer) by Id
    Implemented fetching user by its' id from DB with integration test.

--- users/src/test/java/com/movieapp/users/UserControllerTest.java
+++ users/src/test/java/com/movieapp/users/UserControllerTest.java
@@ -1,11 +1,12 @@
 package com.movieapp.users;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -19,6 +20,9 @@ class UserControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @BeforeEach
     void setUp() {
@@ -48,7 +52,35 @@ class UserControllerTest {
         UserRegisterRequest request = new UserRegisterRequest(email, password, firstName, lastName);
         webClient.getResponseSpecForRequestBody(request)
                 .expectStatus()
-                .isBadRequest();
+                .isBadRequest(); // todo: get response's body
+    }
+
+    @Test
+    void shouldFindUserById() {
+        UserRegisterRequest request = new UserRegisterRequest("testuser@gmail.com", "pass123", "Test", "User");
+        UserDTO register = userService.register(request);
+        Long id = register.id();
+        EntityExchangeResult<UserDTO> userDTOEntityExchangeResult = webClient.requestSpecForGetById(id.intValue())
+                .expectStatus()
+                .isOk()
+                .expectBody(UserDTO.class)
+                .returnResult();
+
+        UserDTO userDTO = userDTOEntityExchangeResult.getResponseBody();
+        validateUser(request, userDTO);
+    }
+
+    @Test
+    void shouldThrowWhenFindingNonExistingId() {
+        UserRegisterRequest request = new UserRegisterRequest("testuser@gmail.com", "pass123", "Test", "User");
+        UserDTO register = userService.register(request);
+        int id = register.id().intValue() + 10000;
+
+        EntityExchangeResult<RestExceptionMessage> restExceptionMessageEntityExchangeResult = webClient.requestSpecForGetById(id)
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(RestExceptionMessage.class)
+                .returnResult();
     }
 
     private void validateUser(UserRegisterRequest request, UserDTO user) {
