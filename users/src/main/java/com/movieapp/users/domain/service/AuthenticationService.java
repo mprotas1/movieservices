61033d9d711372aba40cba0183f11a5dd2f97150
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-08-24 18:46:21 +0200

    Authentication domain layer

--- users/src/main/java/com/movieapp/users/domain/service/AuthenticationService.java
+++ users/src/main/java/com/movieapp/users/domain/service/AuthenticationService.java
@@ -1,11 +1,12 @@
 package com.movieapp.users.domain.service;
 
+import com.movieapp.users.web.dto.UserAuthenticationResponse;
 import com.movieapp.users.web.dto.UserDTO;
 import com.movieapp.users.web.dto.UserLoginRequest;
 import com.movieapp.users.web.dto.UserRegisterRequest;
 import jakarta.validation.Valid;
 
 public interface AuthenticationService {
-    UserDTO register(@Valid UserRegisterRequest request);
-    UserDTO authenticate(UserLoginRequest request);
+    UserAuthenticationResponse register(@Valid UserRegisterRequest request);
+    UserAuthenticationResponse authenticate(UserLoginRequest request);
 }
--- users/src/main/java/com/movieapp/users/domain/service/StandardAuthenticationService.java
+++ users/src/main/java/com/movieapp/users/domain/service/StandardAuthenticationService.java
@@ -1,52 +1,68 @@
 package com.movieapp.users.domain.service;
 
-import com.movieapp.users.domain.mapper.UserMapper;
 import com.movieapp.users.domain.entity.RoleType;
 import com.movieapp.users.domain.entity.User;
+import com.movieapp.users.domain.exception.FailedAuthenticationException;
+import com.movieapp.users.domain.mapper.UserMapper;
 import com.movieapp.users.domain.repository.UserRepository;
+import com.movieapp.users.web.dto.UserAuthenticationResponse;
 import com.movieapp.users.web.dto.UserDTO;
 import com.movieapp.users.web.dto.UserLoginRequest;
 import com.movieapp.users.web.dto.UserRegisterRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-class StandardAuthenticationService implements AuthenticationService, UserDetailsService {
+class StandardAuthenticationService implements AuthenticationService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final RoleService roleService;
+    private final TokenService tokenService;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     @Transactional
-    public UserDTO register(@Valid UserRegisterRequest request) {
+    public UserAuthenticationResponse register(@Valid UserRegisterRequest request) {
         log.debug("Registering the user with email address: {}", request.email());
         User user = userMapper.toEntity(request);
         User registered = userRepository.save(user);
         User userWithRole = roleService.addToRole(registered, RoleType.USER);
-        UserDTO dto = userMapper.toDTO(userWithRole);
-        log.debug("Successfully create user with e-mail: {}", dto.email());
-        return dto;
+        String token = tokenService.generateToken(userWithRole);
+        UserDTO userDTO = userMapper.toDTO(userWithRole);
+        log.debug("Successfully created user with e-mail: {}", userWithRole.getEmail());
+        return new UserAuthenticationResponse(userDTO, token);
     }
 
     @Override
-    public UserDTO authenticate(@Valid UserLoginRequest request) {
+    @Transactional
+    public UserAuthenticationResponse authenticate(@Valid UserLoginRequest request) {
         log.debug("Authenticating the user with email address: {}", request.email());
-        return null;
+        User details = (User) userDetailsService.loadUserByUsername(request.email());
+        if (passwordIsInvalid(request, details)) {
+            log.debug("Could not authenticate user with e-mail: {}", request.email());
+            throw new FailedAuthenticationException("Authentication of the user: " + request.email() + " failed");
+        }
+        String token = tokenService.generateToken(details);
+        log.debug("Successfully authenticated user with e-mail: {}", request.email());
+        UserDTO dto = userMapper.toDTO(details);
+        return new UserAuthenticationResponse(dto, token);
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository
-                .findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User with email: " + email + " not found"));
+    private boolean passwordIsInvalid(UserLoginRequest request, UserDetails details) {
+        return !passwordEncoder.matches(request.password(), details.getPassword());
     }
 
 }
