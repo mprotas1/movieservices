9a0e397721a1fc7aa1005f57ac97a7329a3a3f4a
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-09-12 23:11:53 +0200

    Cinema creation tests and MySQL testcontainers
    Cinema creation integration tests (positive and negative results).
    Introduced scenario for checking whether Cinema exists by name.

--- cinemas/src/test/java/com/movieapp/cinemas/domain/service/CinemaIntegrationTest.java
+++ cinemas/src/test/java/com/movieapp/cinemas/domain/service/CinemaIntegrationTest.java
@@ -4,6 +4,8 @@ import com.movieapp.cinemas.domain.model.AddressInformation;
 import com.movieapp.cinemas.domain.model.CinemaDTO;
 import com.movieapp.cinemas.domain.model.CinemaInformation;
 import com.movieapp.cinemas.testcontainers.Containers;
+import jakarta.persistence.EntityExistsException;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,19 +19,34 @@ class CinemaIntegrationTest extends Containers {
 
     @Test
     void shouldCreateCinemaWithValidData() {
-        // given
         CinemaInformation cinemaInformation = new CinemaInformation("Cinema Name",
                                               new AddressInformation("Blank Street", "Blank City", "00-000")
         );
 
-        // when
         CinemaDTO cinema = cinemaService.createCinema(cinemaInformation);
 
-        // then
         assertNotNull(cinema);
         assertNotNull(cinema.id());
         assertNotNull(cinema.name());
         assertEquals("Cinema Name", cinema.name());;
     }
 
+
+    @Test
+    void shouldNotCreateCinemaWithBlankName() {
+        CinemaInformation cinemaInformation = new CinemaInformation("",
+                                              new AddressInformation("Blank Street", "Blank City", "00-000")
+        );
+
+        assertThrows(ConstraintViolationException.class, () -> cinemaService.createCinema(cinemaInformation));
+    }
+
+    @Test
+    void shouldNotCreateCinemaWithExistingName() {
+        CinemaInformation cinemaInformation = new CinemaInformation("Cinema Name",
+                                              new AddressInformation("Blank Street", "Blank City", "00-000")
+        );
+
+        cinemaService.createCinema(cinemaInformation);
+        assertThrows(EntityExistsException.class, () -> cinemaService.createCinema(cinemaInformation)); }
 }
