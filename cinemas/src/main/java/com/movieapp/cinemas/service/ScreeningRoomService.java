aa2ce4d30da944a4bfe9cb60777f6586d7536944
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-10-04 16:15:16 +0200

    Added logs to ScreeningRoomService

--- cinemas/src/main/java/com/movieapp/cinemas/service/ScreeningRoomService.java
+++ cinemas/src/main/java/com/movieapp/cinemas/service/ScreeningRoomService.java
@@ -10,6 +10,8 @@ import com.movieapp.cinemas.domain.repository.CinemaRepository;
 import com.movieapp.cinemas.domain.repository.CinemaRoomRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,18 +22,22 @@ import java.util.function.Predicate;
 @Service
 @RequiredArgsConstructor
 class ScreeningRoomService implements CinemaRoomService {
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
     private final CinemaRepository cinemaRepository;
     private final CinemaRoomRepository cinemaRoomRepository;
 
     @Override
     @Transactional
     public CinemaRoomDTO addRoom(CinemaRoomInformation roomInformation) {
+        log.debug("Attempting to add room with capacity {} to cinema with id {}", roomInformation.capacity(), roomInformation.cinemaId());
         Cinema contextCinema = cinemaRepository.findById(new CinemaId())
                 .orElseThrow(() -> new EntityNotFoundException("Cinema with id " + roomInformation.cinemaId() + " not found"));
         int roomNumber = contextCinema.getNextRoomNumber();
+        log.debug("Adding room with number {} to cinema with id {}", roomNumber, roomInformation.cinemaId());
         CinemaRoom room = new CinemaRoom(roomNumber, roomInformation.capacity(), contextCinema);
         CinemaRoom savedRoom = cinemaRoomRepository.save(room);
         UUID cinemaId = savedRoom.getCinema().getIdValue();
+        log.debug("Room with number {} added to cinema with id {}", roomNumber, cinemaId);
         return new CinemaRoomDTO(cinemaId, savedRoom.getNumber(), savedRoom.getCapacity());
     }
 
@@ -39,15 +45,16 @@ class ScreeningRoomService implements CinemaRoomService {
     public CinemaRoomDTO updateCapacity(Long roomId, int newCapacity) {
         CinemaRoom room = cinemaRoomRepository.findById(new CinemaRoomId(roomId).getValue())
                 .orElseThrow(() -> new EntityNotFoundException("Room with id " + roomId + " not found"));
+        log.debug("Updating capacity for room with id {} from {} to {}", roomId, room.getCapacity(), newCapacity);
         room.updateCapacity(newCapacity);
         CinemaRoom updatedRoom = cinemaRoomRepository.save(room);
         UUID cinemaId = updatedRoom.getCinema().getIdValue();
+        log.debug("Capacity for room with id {} updated to {}", roomId, newCapacity);
         return new CinemaRoomDTO(cinemaId, updatedRoom.getNumber(), updatedRoom.getCapacity());
     }
 
     @Override
     public List<CinemaRoomDTO> findByCinemaId(UUID cinemaId) {
-
         return cinemaRoomRepository.findByCinemaId(cinemaId.toString()).stream()
                 .map(room -> new CinemaRoomDTO(room.getCinema().getIdValue(), room.getNumber(), room.getCapacity()))
                 .toList();
@@ -55,11 +62,13 @@ class ScreeningRoomService implements CinemaRoomService {
 
     @Override
     public void deleteRoom(Long roomId) {
+        log.debug("Attempting to delete room with id {}", roomId);
         cinemaRoomRepository.deleteById(roomId);
     }
 
     @Override
     public void deleteByNumber(UUID cinemaId, int roomNumber) {
+        log.debug("Attempting to delete room with number: {} from cinema with id: {}", roomNumber, cinemaId);
         cinemaRoomRepository.findByCinemaId(cinemaId.toString()).stream()
                 .filter(isSameNumber(roomNumber))
                 .findFirst()
