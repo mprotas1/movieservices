4c3e3c4055b50ec7ed92aa17d215e6c57740cc4a
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-10-12 21:35:20 +0200

    Seats entity DDL

---
+++ cinemas/src/main/resources/db/migration/V5__cinemas.sql
@@ -0,0 +1,12 @@
+ALTER TABLE cinema_rooms DROP COLUMN `id`;
+ALTER TABLE cinema_rooms ADD COLUMN `uuid` BINARY(16) NOT NULL;
+ALTER TABLE cinema_rooms ADD PRIMARY KEY (`uuid`);
+
+CREATE TABLE IF NOT EXISTS `seats` (
+    `id` BINARY(16) UNIQUE NOT NULL PRIMARY KEY,
+    `row` INTEGER NOT NULL,
+    `seat_number` INTEGER NOT NULL,
+    `cinema_room_id` BINARY NOT NULL
+);
+
+ALTER TABLE `seats` ADD CONSTRAINT `fk_seats_cinema_rooms` FOREIGN KEY (`cinema_room_id`) REFERENCES `cinema_rooms` (`uuid`);
