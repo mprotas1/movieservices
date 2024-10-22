dff3b78c0c5e2023c0ef651d46fccf98ea0848da
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-10-22 18:47:19 +0200

    Update CinemaDTO.java

--- cinemas/src/main/java/com/movieapp/cinemas/service/model/CinemaDTO.java
+++ cinemas/src/main/java/com/movieapp/cinemas/service/model/CinemaDTO.java
@@ -3,15 +3,19 @@ package com.movieapp.cinemas.service.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.movieapp.cinemas.domain.entity.Address;
 import com.movieapp.cinemas.domain.entity.Cinema;
+import com.movieapp.cinemas.domain.entity.Coordinates;
 
 import java.util.UUID;
 
 public record CinemaDTO(UUID id,
                         String name,
-                        @JsonProperty(value = "address") String formattedAddress) {
+                        @JsonProperty(value = "address") String formattedAddress,
+                        double latitude,
+                        double longitude) {
 
     public static CinemaDTO fromEntity(Cinema cinema) {
-        return new CinemaDTO(cinema.getId().getUuid(), cinema.getName(), getFormattedAddress(cinema.getAddress()));
+        Coordinates coordinates = cinema.getCoordinates();
+        return new CinemaDTO(cinema.getId().getUuid(), cinema.getName(), getFormattedAddress(cinema.getAddress()), coordinates.latitude(), coordinates.longitude());
     }
 
     private static String getFormattedAddress(Address address) {
