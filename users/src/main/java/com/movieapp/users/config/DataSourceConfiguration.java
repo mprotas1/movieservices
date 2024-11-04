4a45640f7857bbfaa1e4de94d2397dd69c00843a
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-11-04 20:51:19 +0100

    Customers Eureka client introduction

---
+++ users/src/main/java/com/movieapp/users/config/DataSourceConfiguration.java
@@ -0,0 +1,44 @@
+package com.movieapp.users.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+class DataSourceConfiguration {
+
+    @Bean
+    @Primary
+    public DataSource postgresqlDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/usersdb")
+                .username("user")
+                .password("secret")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public Flyway flyway(@Qualifier("postgresqlDataSource") DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+    }
+
+    @Bean
+    public ApplicationRunner migrateFlyway(Flyway flyway) {
+        return args -> {
+            flyway.migrate();
+        };
+    }
+
+}
