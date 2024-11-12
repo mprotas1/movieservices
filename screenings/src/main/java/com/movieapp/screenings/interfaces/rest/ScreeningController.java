6521345d3244a2a81ccdeef16c53cab85027bd26
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-11-12 20:07:48 +0100

    Added logs

--- screenings/src/main/java/com/movieapp/screenings/interfaces/rest/ScreeningController.java
+++ screenings/src/main/java/com/movieapp/screenings/interfaces/rest/ScreeningController.java
@@ -4,6 +4,7 @@ import com.movieapp.screenings.application.dto.ScreeningCreateRequest;
 import com.movieapp.screenings.application.dto.ScreeningDTO;
 import com.movieapp.screenings.application.service.ScreeningApplicationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,18 +15,22 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 class ScreeningController {
     private final ScreeningApplicationService screeningService;
 
     @PostMapping
     ResponseEntity<ScreeningDTO> createScreening(@RequestBody ScreeningCreateRequest request) {
+        log.debug("Creating new Screening with request: {}", request);
         var screening = screeningService.createScreening(request);
+        log.debug("Created Screening: {}", screening);
         return ResponseEntity.ok(screening);
     }
 
     @GetMapping
     ResponseEntity<List<ScreeningDTO>> findAll() {
         var screenings = screeningService.findAll();
+        log.debug("Found {} Screenings", screenings.size());
         return ResponseEntity.ok(screenings);
     }
 
