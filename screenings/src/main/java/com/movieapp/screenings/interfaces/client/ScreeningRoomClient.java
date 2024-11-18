5017c8d4b5dd7163d216372964f8fb0ee8bd0fc7
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-11-18 20:46:57 +0100

    ScreeningRoomClient implementation

---
+++ screenings/src/main/java/com/movieapp/screenings/interfaces/client/ScreeningRoomClient.java
@@ -0,0 +1,9 @@
+package com.movieapp.screenings.interfaces.client;
+
+import com.movieapp.screenings.application.dto.ScreeningRoomDTO;
+import com.movieapp.screenings.domain.model.ScreeningRoom;
+import com.movieapp.screenings.domain.model.ScreeningRoomId;
+
+public interface ScreeningRoomClient {
+    ScreeningRoomDTO getScreeningRoomById(ScreeningRoomId id);
+}
