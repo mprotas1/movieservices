df339998422d31383de965b2ee6a97cbf4bd63ff
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-11-19 19:21:45 +0100

    Update ScreeningRoomClient.java

--- screenings/src/main/java/com/movieapp/screenings/interfaces/client/ScreeningRoomClient.java
+++ screenings/src/main/java/com/movieapp/screenings/interfaces/client/ScreeningRoomClient.java
@@ -1,7 +1,6 @@
 package com.movieapp.screenings.interfaces.client;
 
 import com.movieapp.screenings.application.dto.ScreeningRoomDTO;
-import com.movieapp.screenings.domain.model.ScreeningRoom;
 import com.movieapp.screenings.domain.model.ScreeningRoomId;
 
 public interface ScreeningRoomClient {
