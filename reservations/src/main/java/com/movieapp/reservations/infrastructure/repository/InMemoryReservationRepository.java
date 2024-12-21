8d70bb2eb2ad86bfa27eebe0d772b72931cbc1dc
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-12-21 19:36:38 +0100

    InMemoryReservationRepository implementation

---
+++ reservations/src/main/java/com/movieapp/reservations/infrastructure/repository/InMemoryReservationRepository.java
@@ -0,0 +1,58 @@
+package com.movieapp.reservations.infrastructure.repository;
+
+import com.movieapp.reservations.domain.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class InMemoryReservationRepository implements ReservationRepository {
+    private final Map<ReservationId, Reservation> reservations = new HashMap<>();
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        reservations.put(reservation.getReservationId(), reservation);
+        return reservation;
+    }
+
+    @Override
+    public Optional<Reservation> findById(ReservationId id) {
+        return Optional.ofNullable(reservations.get(id));
+    }
+
+    @Override
+    public Optional<Reservation> findByScreeningIdAndSeatId(ScreeningId screeningId, SeatId seatId) {
+        return reservations.values().stream()
+                .filter(reservation -> reservation.getScreeningId().equals(screeningId) && reservation.getSeatId().equals(seatId))
+                .findFirst();
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return new ArrayList<>(reservations.values());
+    }
+
+    @Override
+    public List<Reservation> findByScreeningId(ScreeningId screeningId) {
+        return reservations.values().stream()
+                .filter(reservation -> reservation.getScreeningId().equals(screeningId))
+                .toList();
+    }
+
+    @Override
+    public List<Reservation> findByUserId(UserId userId) {
+        return reservations.values().stream()
+                .filter(reservation -> reservation.getUserId().equals(userId))
+                .toList();
+    }
+
+    @Override
+    public void delete(Reservation reservation) {
+        reservations.remove(reservation.getReservationId());
+    }
+
+    @Override
+    public void deleteById(ReservationId id) {
+        reservations.remove(id);
+    }
+}
