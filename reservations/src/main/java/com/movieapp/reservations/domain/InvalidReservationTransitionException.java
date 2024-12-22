f37df9c19423d58d252fb14e68e7f351ab04fb5c
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2024-12-22 13:46:36 +0100

    Update InvalidReservationTransitionException.java

--- reservations/src/main/java/com/movieapp/reservations/domain/InvalidReservationTransitionException.java
+++ reservations/src/main/java/com/movieapp/reservations/domain/InvalidReservationTransitionException.java
@@ -2,7 +2,7 @@ package com.movieapp.reservations.domain;
 
 public class InvalidReservationTransitionException extends RuntimeException {
 
-        public InvalidReservationTransitionException(String message) {
+    public InvalidReservationTransitionException(String message) {
             super(message);
         }
 
