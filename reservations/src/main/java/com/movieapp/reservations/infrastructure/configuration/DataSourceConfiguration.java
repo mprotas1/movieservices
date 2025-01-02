df9253feb81ebee44f484e972f966b3351954c24
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2025-01-02 18:35:30 +0100

    Reservations properties change

---
+++ reservations/src/main/java/com/movieapp/reservations/infrastructure/configuration/DataSourceConfiguration.java
@@ -0,0 +1,28 @@
+package com.movieapp.reservations.infrastructure.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+class DataSourceConfiguration {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+            .url(url)
+            .username(username)
+            .password(password)
+            .build();
+    }
+
+}
