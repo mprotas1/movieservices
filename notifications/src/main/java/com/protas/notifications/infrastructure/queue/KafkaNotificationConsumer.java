d508341004f612557bf1fc792ad3017d398426ff
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2025-02-13 21:23:20 +0100

    Notification consumer implementation

--- notifications/src/main/java/com/protas/notifications/infrastructure/queue/KafkaNotificationConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.protas.notifications.infrastructure.queue;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class KafkaNotificationConsumer {
-}
--- notifications/src/main/java/com/protas/notifications/infrastructure/queue/NotificationConsumer.java
+++ notifications/src/main/java/com/protas/notifications/infrastructure/queue/NotificationConsumer.java
@@ -1,4 +1,7 @@
 package com.protas.notifications.infrastructure.queue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public interface NotificationConsumer {
+    void handleNotificationRequest(String notificationRequest) throws JsonProcessingException;
 }
