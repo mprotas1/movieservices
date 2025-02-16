328b60e96888f5175a2802b0fb9413fd2d70fec1
Michał Protas <83657949+mprotas1@users.noreply.github.com>
2025-02-16 21:20:32 +0100

    Screenings service Kafka properties

--- screenings/src/main/java/com/movieapp/screenings/infrastructure/configuration/KafkaConsumerConfiguration.java
+++ screenings/src/main/java/com/movieapp/screenings/infrastructure/configuration/KafkaConsumerConfiguration.java
@@ -2,6 +2,7 @@ package com.movieapp.screenings.infrastructure.configuration;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -15,6 +16,8 @@ import java.util.Map;
 
 @Configuration
 class KafkaConsumerConfiguration {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory(
@@ -32,7 +35,7 @@ class KafkaConsumerConfiguration {
 
     private Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
--- screenings/src/main/java/com/movieapp/screenings/infrastructure/configuration/KafkaProducerConfiguration.java
+++ screenings/src/main/java/com/movieapp/screenings/infrastructure/configuration/KafkaProducerConfiguration.java
@@ -2,6 +2,7 @@ package com.movieapp.screenings.infrastructure.configuration;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -12,6 +13,8 @@ import java.util.Map;
 
 @Configuration
 class KafkaProducerConfiguration {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
@@ -25,7 +28,7 @@ class KafkaProducerConfiguration {
 
     private Map<String, Object> producerProps() {
         return Map.of(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
         );
